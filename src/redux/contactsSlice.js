cced8e610da43eb29a2e7b86f5044d36a8e70f4d
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-12-06 14:22:57 +0100

    delete reducers

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -17,16 +17,6 @@ const initialState = {
 const slice = createSlice({
   name: "contacts",
   initialState,
-  reducers: {
-    deleteContact: (state, action) => {
-      state.contacts.items = state.contacts.items.filter(
-        (item) => item.id !== action.payload
-      );
-    },
-    addContact: (state, action) => {
-      state.contacts.items.push(action.payload);
-    },
-  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchContacts.fulfilled, (state, action) => {

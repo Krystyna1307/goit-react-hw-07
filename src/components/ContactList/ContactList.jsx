ce6c464baae39cdb578d2bf3de19ba3610eb1dc5
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-12-08 15:23:35 +0100

    create select FilteredContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,15 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
-import { selectContacts, selectFilter } from "../../redux/selectors";
+import { selectFilteredContacts } from "../../redux/selectors";
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-
-  const selectFilteredName = contacts.filter((item) =>
-    item.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <div className={s.wrapper}>
       <ul className={s.list}>
-        {selectFilteredName.map((item) => (
+        {filteredContacts.map((item) => (
           <Contact {...item} key={item.id} />
         ))}
       </ul>
